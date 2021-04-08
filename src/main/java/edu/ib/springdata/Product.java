c26d063f372e6e1c59a0214d9f511bdccc4212fd
damiapaw99 <damianpaw99@gmail.com>
2021-04-08 20:51:47 +0200

    HB product database

---
+++ src/main/java/edu/ib/springdata/Product.java
@@ -0,0 +1,61 @@
+package edu.ib.springdata;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private float price;
+
+    private boolean available;
+
+    public Product() {
+    }
+
+    public Product(String name, float price, boolean available) {
+        this.name = name;
+        this.price = price;
+        this.available = available;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
