3564c19e43f85c4aa9ffb94b665aa0c04a1ef231
damiapaw99 <damianpaw99@gmail.com>
2021-06-20 16:25:23 +0200

    Updated env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,6 @@ export AZ_LOCATION=eastus
 export AZ_MYSQL_USERNAME=spring
 export AZ_MYSQL_PASSWORD=password
 export AZ_LOCAL_IP_ADDRESS=$(curl http://whatismyip.akamai.com/)
-export SPRING_DATASOURCE_URL=
-jdbc:mysql://$AZ_DATABASE_NAME.mysql.database.azure.com:3306/demo?serverTimezone=UTC
+export SPRING_DATASOURCE_URL=jdbc:mysql://$AZ_DATABASE_NAME.mysql.database.azure.com:3306/demo?serverTimezone=UTC
 export SPRING_DATASOURCE_USERNAME=spring@$AZ_DATABASE_NAME
 export SPRING_DATASOURCE_PASSWORD=$AZ_MYSQL_PASSWORD
